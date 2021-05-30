50204fb42c89674990afea57467e1caf8496a1be
DimitriBelikov <urmilchandarana@gmail.com>
2021-05-30 18:27:50 +0530

    Server Functionalities Updates -> GET audio and multi-user specs

--- Web-Server/server.py
+++ Web-Server/server.py
@@ -1,20 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 import os
 from werkzeug.utils import secure_filename
-from services import InputToOutputAudio
+from services import InputToOutputAudio, RandomTextGenerator
+from json import dumps
 
 server_app = Flask(__name__)
 services = InputToOutputAudio()
 
-@server_app.route('/translate_voice', methods=['POST'])
+@server_app.route('/translate-voice', methods=['POST'])
 def translate_voice():
     audio_file = request.files['audio_file']
-    print(type(audio_file))
-    audio_filename = secure_filename(audio_file.filename)
-    audio_file_path = os.path.join(os.getcwd(), 'audio_data', audio_filename)
-    
-    audio_file.save(audio_file_path)
+    audio_filename = RandomTextGenerator().generate_random_text() + audio_file.filename[audio_file.filename.rindex('.'):]
+    audio_filepath = os.path.join(os.getcwd(), 'audio_data', audio_filename)
+
+    print('\nAudio : '+ audio_filename)
+    audio_file.save(audio_filepath)
 
     itoa = InputToOutputAudio()
-    output_audio = itoa.inputToOutputAudio(audio_data = audio_file_path)
-    return 'HTTP 200'
+    response, output_audio = itoa.inputToOutputAudio(audio_path = audio_filepath)
+
+    html_responsedata = {'Response': response, 'redirect_path': audio_filename[:audio_filename.rindex('.')]}
+    html_response = dumps(html_responsedata)
+
+    return html_response
+
+@server_app.route('/output-audio/<audiofile>', methods=['GET'])
+def getaudio(audiofile):
+    audio_filename = audiofile + '.mp3'
+    audio_directory = os.path.join(os.getcwd(), 'output_data')
+
+    return send_from_directory(audio_directory, path=audio_filename)
--- Web-Server/services.py
+++ Web-Server/services.py
@@ -3,6 +3,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import google_trans_new as gtn
 import gtts
 import os
+from random import randint
 
 sttapikey = 'wW82XoUPgI4tey3TfmY4LKiEeib_iqcJ5FwFt54PyTS_'
 stturl = 'https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/e4d9d9ef-9aa9-4f68-a9bb-5ce2f38bf147'
@@ -33,11 +34,30 @@ class InputToOutputAudio:
         output_audio = gtts.gTTS(translated_text, lang=tgt_language)
         return output_audio
     
-    def inputToOutputAudio(self, audio_data, to_lang='en-us', tgt_lang='hi'):
-        recog_audio_text = self.__inputAudioToText(audio_data)
+    def inputToOutputAudio(self, audio_path, to_lang='en-us', tgt_lang='hi'):
+        recog_audio_text = self.__inputAudioToText(audio_path)
         translated_text = self.__translateText(recog_audio_text, to_lang, tgt_lang)
         output_audio = self.__translatedTexttoOutputAudio(translated_text, tgt_lang)
 
-        output_audio_path = os.path.join(os.getcwd(), 'output_data', 'sample.mp3')
+        output_filename = audio_path[audio_path.rindex('\\')+1:]
+        output_audio_path, response = os.path.join(os.getcwd(), 'output_data', output_filename), 'HTTP 200'
         output_audio.save(output_audio_path)
-        return output_audio_path
+        print('========================================\n')
+        
+        return response, output_audio_path
+
+class RandomTextGenerator:
+
+    def generate_random_text(self):
+        filename_length = randint(10,15)
+        filename = ''
+        for x in range(1, filename_length+1):
+            random_2, random_3 = randint(1,10), 99
+            
+            if random_2 < 6:
+                random_3 = str(randint(1,9))
+            else:
+                random_3 = chr(randint(65,91))
+
+            filename += random_3
+        return filename
